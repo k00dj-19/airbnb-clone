1b45af9ae3230ed6a5cfe0738434f4f75b3fe780
k00dj-19 <rlaehdwls310@khu.ac.kr>
2020-07-19 17:42:55 +0900

    #3.1 Introduction to the User Model

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,4 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-
 AUTH_USER_MODEL = "users.User"
-
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
-    pass
+
+    bio = models.TextField(default="")

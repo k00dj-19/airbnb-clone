b01a35879ddadc07d316f4357079b90272afb0cd
k00dj-19 <rlaehdwls310@khu.ac.kr>
2020-07-20 15:49:56 +0900

    #4.1 Room Model part1

--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
 class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract = True  
-        #abstract model == 데이터베이스에는 저장되지 않는 모델
+        abstract = True
+        # abstract model == 데이터베이스에는 저장되지 않는 모델
+

a981c1c1f1991ac44470a3b8793e2deaf41e2710
k00dj-19 <rlaehdwls310@khu.ac.kr>
2020-07-21 20:47:55 +0900

    7. Models and QuerySets #7.0 Managers and QuerySets / #7.1 Practicing the Django ORM

--- rooms/admin.py
+++ rooms/admin.py
@@ -72,8 +72,7 @@ class RoomAdmin(admin.ModelAdmin):
     )
 
     def count_amenities(self, obj):
-        print(obj.amenities.all())
-        return "Potato"
+        return obj.amenities.count()
 
     count_amenities.short_description = "Hello Sexy!"
 
